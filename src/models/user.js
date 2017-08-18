601e93f07ec597f8282547a1c7707e0e5afc007d
forforech <dima.kuzmich.work@gmail.com>
2017-08-18 11:32:11 +0300

    feat(user): add user model

--- src/models/user.js
+++ src/models/user.js
@@ -1,1 +1,26 @@
 const mongoose = require('mongoose');
+const isEmail = require('validator').isEmail;
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  firstName: String,
+  LastName: String,
+  userName: {
+    type: String,
+    required: 'Username is required'
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: 'Email is required for user',
+    validate: {
+      validator: isEmail,
+      msg: 'Email has incorrect format'
+    }
+  }
+}, { timestamps: true });
+
+const User = mongoose.model('user', UserSchema);
+
+module.exports = User;
