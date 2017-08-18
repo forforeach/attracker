7b2f11b7059d96246bb3ee847983888a1df2434a
forforech <dima.kuzmich.work@gmail.com>
2017-08-18 12:01:27 +0300

    test(helper): add helper to prepare env for testing

---
+++ test/test.helper.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const dbConfig = require('./../configs/app.config').db;
+
+const promisifyDrop = (func) => new Promise((resolve) => {
+  func.apply(null, () => resolve());
+});
+
+before((done) => {
+  mongoose.connect(dbConfig.connectionString, {
+    useMongoClient: true,
+  });
+
+  mongoose.connection
+    .once('open', () => done())
+    .on('error', (error) => {
+      console.error(error);
+    });
+});
+
+beforeEach((done) => {
+  const { collections } = mongoose.connection;
+  const collectionsPromises = collections.map((collection) => promisifyDrop(collection.drop));
+  Promise.all(collectionsPromises).then(() => done());
+});
