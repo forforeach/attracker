1766d62cc7a3ddd81fc66012409beced1b80ba56
forforech <dima.kuzmich.work@gmail.com>
2017-08-19 14:25:07 +0300

    test(user): add tests for user controller

--- test/controllers/user.controller.test.js
+++ test/controllers/user.controller.test.js
@@ -13,7 +13,8 @@ describe('User controller', () => {
       email: 'test@test.com',
       userName: 'username',
       firstName: 'test',
-      lastName: 'user'
+      lastName: 'user',
+      password: 'password'
     };
     request(app)
       .post('/api/users')
@@ -25,6 +26,86 @@ describe('User controller', () => {
             expect(user.email).to.be.equal(userProps.email);
             done();
           }).catch((error) => console.log(error));
+      });
+  });
+
+  it('GET to /api/users/:id should retrieve a user', (done) => {
+    const user = new User({
+      email: 'test@test.com',
+      userName: 'username',
+      firstName: 'test',
+      lastName: 'user',
+      password: 'password'
+    });
+    user.save()
+      .then(() => {
+        request(app)
+          .get(`/api/users/${user._id}`)
+          .then((res) => {
+            expect(res.body).not.to.be.null;
+            expect(res.body.email).to.be.equal(user.email);
+            expect(res.body._id.toString()).to.be.equal(user._id.toString());
+            done();
+          });
+      });
+  });
+
+  it('GET to /api/users should retrieve all users', (done) => {
+    const user = new User({
+      email: 'test@test.com',
+      userName: 'username',
+      firstName: 'test',
+      lastName: 'user',
+      password: 'password'
+    });
+    user.save()
+      .then(() => {
+        request(app)
+          .get(`/api/users`)
+          .then((res) => {
+            expect(res.body).not.to.be.null;
+            expect(res.body).to.be.an('array');
+            expect(res.body.length).to.be.equal(1);
+            expect(res.body[0]._id.toString()).to.be.equal(user._id.toString());
+            done();
+          });
+      });
+  });
+
+  it('PUT to /api/users/:id should update a user', (done) => {
+    const user = new User({
+      email: 'test@test.com',
+      userName: 'username',
+      firstName: 'test',
+      lastName: 'user',
+      password: 'password'
+    });
+    user.save()
+      .then(() => request(app)
+        .put(`/api/users/${user._id}`)
+        .send({ firstName: 'Joe' }))
+      .then(() => User.findById(user._id))
+      .then((dbUser) => {
+        expect(dbUser.firstName).to.be.equal('Joe');
+        done();
+      });
+  });
+
+  it('DELETE to /api/users/:id should remove a user', (done) => {
+    const user = new User({
+      email: 'test@test.com',
+      userName: 'username',
+      firstName: 'test',
+      lastName: 'user',
+      password: 'password'
+    });
+    user.save()
+      .then(() => request(app)
+        .delete(`/api/users/${user._id}`))
+      .then(() => User.findById(user._id))
+      .then((dbUser) => {
+        expect(dbUser).to.be.null;
+        done();
       });
   });
 });
