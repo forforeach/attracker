aedde4701d8a53462deed07b15207e46aa8b8d0d
forforech <dima.kuzmich.work@gmail.com>
2017-08-21 17:58:47 +0300

    Updates

--- apidoc/api_project.js
+++
@@ -1,1 +0,0 @@
-define({  "name": "Attracker",  "version": "0.0.1",  "description": "Attracker API documentation",  "title": "Attracker API",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2017-08-21T14:32:10.459Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});
