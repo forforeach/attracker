f20a6bbf8e1367fdf0347425b09f147768851dc9
forforeach <dima.kuzmich.work@gmail.com>
2018-06-30 21:41:48 +0300

    fix(login): fix login form to submit on enter

--- services/web/src/components/login/Login.vue
+++ services/web/src/components/login/Login.vue
@@ -4,15 +4,15 @@
       <v-toolbar-title>Login</v-toolbar-title>
     </v-toolbar>
     <v-card-text>
-      <v-form>
+      <v-form @submit="login">
         <v-text-field prepend-icon="person" name="username" label="User name" type="text" v-model="username"></v-text-field>
         <v-text-field prepend-icon="lock" name="password" label="Password" type="password" v-model="password"></v-text-field>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn type="submit" color="primary">Login</v-btn>
+        </v-card-actions>
       </v-form>
     </v-card-text>
-    <v-card-actions>
-      <v-spacer></v-spacer>
-      <v-btn color="primary" @click="login">Login</v-btn>
-    </v-card-actions>
   </v-card>
 </template>
 
@@ -28,7 +28,8 @@ export default {
     }
   },
   methods: {
-    login () {
+    login (event) {
+      event.preventDefault()
       this.$store.dispatch({
         type: AUTH_LOGIN_ACTION,
         payload: { username: this.username, password: this.password }
