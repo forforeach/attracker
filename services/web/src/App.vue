ffadde8182953a1fe760658097a7bd3a4c2623f1
forforeach <dima.kuzmich.work@gmail.com>
2018-06-30 21:40:38 +0300

    feat(app):  hide header if not looged in

--- services/web/src/App.vue
+++ services/web/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <v-app>
-      <att-header></att-header>
+      <att-header v-if="loggedIn"></att-header>
       <v-content>
         <v-container fluid>
           <router-view/>
@@ -10,12 +10,21 @@
     </v-app>
   </div>
 </template>
+
 <script>
 import AttHeader from '@/components/header/Header.vue'
+import { mapState } from 'vuex'
+import { AUTH_LOGEDIN } from '@/stores/auth.js'
+
 export default {
   name: 'app',
   components: {
     AttHeader
+  },
+  computed: {
+    ...mapState({
+      loggedIn: state => state.auth[AUTH_LOGEDIN]
+    })
   }
 }
 </script>
--- services/web/src/components/header/Header.vue
+++ services/web/src/components/header/Header.vue
@@ -1,21 +1,13 @@
 <template>
   <v-toolbar app>
     <v-toolbar-items>
-      <v-btn v-if="loggedIn" flat to="/">Home</v-btn>
-      <v-btn v-if="!loggedIn" flat to="/login">Login</v-btn>
+      <v-btn flat to="/">Home</v-btn>
     </v-toolbar-items>
   </v-toolbar>
 </template>
 
 <script>
-import { mapState } from 'vuex'
-import { AUTH_LOGEDIN } from '@/stores/auth.js'
 export default {
-  name: 'att-header',
-  computed: {
-    ...mapState({
-      loggedIn: state => state.auth[AUTH_LOGEDIN]
-    })
-  }
+  name: 'att-header'
 }
 </script>
