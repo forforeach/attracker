70f9b9da32db968c5b9fd14618da53dfebdb17d0
forforeach <dima.kuzmich.work@gmail.com>
2018-07-08 14:43:54 +0300

    feat(build): run tests on CI only for changed services

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,23 +1,41 @@
 #!/bin/bash
 
+TRAVIS_COMMIT_RANGE="${TRAVIS_COMMIT_RANGE:-HEAD HEAD~1}"
+
+echo "Diff range: $TRAVIS_COMMIT_RANGE"
+
+GIT_COMMAND="git"
+GIT_DIFF_PARAMS=(diff --name-only --exit-code $TRAVIS_COMMIT_RANGE)
+
+DIFF=`$GIT_COMMAND ${GIT_DIFF_PARAMS[@]}`
+
 set -e
 
-CHANGED_FILES=($(git diff --name-only HEAD HEAD~1))
+SERVICES=()
+for DIR in $DIFF; do
+  SERVICE=$(echo $DIR | cut -d'/' -f 2)
+  if [[ ! " ${SERVICES[@]} " =~ " ${SERVICE} " ]]; then
+    SERVICES+=($SERVICE)
+  fi
+done
 
-echo "$CHANGED_FILES"
+echo "Changed services: $SERVICES"
 
 TEST_SCRIPT_NAME=test.sh
 ROOT_DIR=$PWD
 for DIR in services/*/; do
-  TEST_SCRIPT_DIR=${DIR}scripts/
-  TEST_SCRIPT_FULL_PATH=$TEST_SCRIPT_DIR$TEST_SCRIPT_NAME
-  if [ -f "$TEST_SCRIPT_FULL_PATH" ]; then
-    printf "\n$DIR"
-    eval "chmod u+x $TEST_SCRIPT_FULL_PATH"
-    printf "\n\tSwitching to $TEST_SCRIPT_DIR ...\n"
-    cd $TEST_SCRIPT_DIR
-    printf "\tRunning before script in $DIR ...\n"
-    bash "$TEST_SCRIPT_NAME"
-    cd $ROOT_DIR
+  SERVICE=$(echo $DIR | cut -d'/' -f 2)
+  if [[ " ${SERVICES[@]} " =~ " ${SERVICE} " ]]; then
+    TEST_SCRIPT_DIR=${DIR}scripts/
+    TEST_SCRIPT_FULL_PATH=$TEST_SCRIPT_DIR$TEST_SCRIPT_NAME
+    if [ -f "$TEST_SCRIPT_FULL_PATH" ]; then
+      printf "\n$DIR"
+      eval "chmod u+x $TEST_SCRIPT_FULL_PATH"
+      printf "\n\tSwitching to $TEST_SCRIPT_DIR ...\n"
+      cd $TEST_SCRIPT_DIR
+      printf "\tRunning before script in $DIR ...\n"
+      bash "$TEST_SCRIPT_NAME"
+      cd $ROOT_DIR
+    fi
   fi
 done
